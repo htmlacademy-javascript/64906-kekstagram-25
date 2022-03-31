f6a53d958048200eac9dcd7c478725e671b88ccf
Вячеслав Николаев <slavkapiyavka@yandex.ru>
2022-04-01 00:12:38 +0300

    Дорабатывает модуль для отрисовки фотографий, чтобы в качестве данных использовались данные с удалённого сервера.
    Добавляет обработку возможных ошибок при загрузке.

--- js/get-data.js
+++ js/get-data.js
@@ -1,5 +1,7 @@
 import {renderPostThumbnails} from './render-post-thumbnails.js';
+import {showNotification} from './utils.js';
 
 fetch('https://25.javascript.pages.academy/kekstagram/data')
   .then((data) => data.json())
-  .then((images) => renderPostThumbnails(images));
+  .then((images) => renderPostThumbnails(images))
+  .catch((err) => showNotification(err));
