97a91b745fc0f91eac7d23062455fb12ba422ed6
Вячеслав Николаев <slavkapiyavka@yandex.ru>
2022-04-06 01:16:12 +0300

    Создаёт и подключает функцию устранения дребезга, но ничего коллбэк из функции никогда не срабатывает

--- js/utils.js
+++ js/utils.js
@@ -36,6 +36,14 @@ const getUniqRandomNumber = (quantity) => {
   return getNextNumber;
 };
 
+const debounce = (callback, timeoutDelay) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {checkStringLength, getRandomPositiveNumber, getUniqRandomNumber, isEscapeKey};
+export {checkStringLength, getRandomPositiveNumber, getUniqRandomNumber, isEscapeKey, debounce};
