6cbae176cfc3a362445c969282d13b4587182e20
Вячеслав Николаев <slavkapiyavka@yandex.ru>
2022-04-07 20:36:54 +0300

    Исправляет функцию задержки фильтрации постов

--- js/filters.js
+++ js/filters.js
@@ -10,35 +10,38 @@ const imageFilterBtnElement = imageFiltersListElement.querySelectorAll('.img-fil
 const picturesContainer = document.querySelector('.pictures');
 
 const filterItems = (photos, filter) => {
-  picturesContainer.querySelectorAll('.picture').forEach((post) => {
-    post.remove();
-  });
+  picturesContainer.querySelectorAll('.picture').forEach((post) => post.remove());
 
-  if(filter.endsWith('default')) {
+  if(filter.id.endsWith('default')) {
     photos.sort((previousPhoto, nextPhoto) => previousPhoto.id - nextPhoto.id);
     renderPostThumbnails(photos);
   }
-  if(filter.endsWith('random')) {
+  if(filter.id.endsWith('random')) {
     photos.sort(() => Math.random() - 0.5);
     renderPostThumbnails(photos.slice(0, RANDOM_PHOTO_MAX_QUANTITY));
   }
-  if(filter.endsWith('discussed')) {
+  if(filter.id.endsWith('discussed')) {
     photos.sort((previousPhoto, nextPhoto) => nextPhoto.comments.length - previousPhoto.comments.length);
     renderPostThumbnails(photos);
   }
 };
 
-const showFilters = (photos) => {
-  imageFiltersElement.classList.remove('img-filters--inactive');
-  imageFiltersListElement.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    imageFilterBtnElement.forEach((btn) => {
-      btn.classList.remove('img-filters__button--active');
-    });
-
-    evt.target.classList.add('img-filters__button--active');
-    debounce(() => filterItems(photos, evt.target.id), DEBOUNCE_TIME);
+const setFilterHandlers = (photos) => {
+  imageFilterBtnElement.forEach((filterBtn) => {
+    filterBtn.addEventListener('click', debounce(() => filterItems(photos, filterBtn), DEBOUNCE_TIME));
   });
 };
+
+const showFilters = (photos) => {
+  imageFiltersElement.classList.remove('img-filters--inactive');
+  setFilterHandlers(photos);
+};
+
+imageFiltersListElement.addEventListener('click', (evt) => {
+  imageFilterBtnElement.forEach((filterBtn) => {
+    filterBtn.classList.remove('img-filters__button--active');
+  });
+  evt.target.classList.add('img-filters__button--active');
+});
 
 export {showFilters};
