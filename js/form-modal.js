28f7a0d5b405e137697541dad40bec108fe4cf36
Вячеслав Николаев <slavkapiyavka@yandex.ru>
2022-04-12 17:02:34 +0300

    Вносит правки по заданию "Фото на память"

--- js/form-modal.js
+++ js/form-modal.js
@@ -1,12 +1,14 @@
 import {isEscapeKey} from './utils.js';
+import {showAlert} from './notifications.js';
 
 const IMAGE_SCALE_VALUE = 100;
 const FILE_TYPES = ['jpeg', 'png', 'jpg', 'gif'];
+const INCORRECT_IMAGE_FORMAT = `Не подходящий формат изображения, используйте следующие форматы: ${FILE_TYPES.join(', ')}`;
 
 const bodyElement = document.body;
-const uploadControlElement = document.querySelector('#upload-file'); // *
+const uploadControlElement = document.querySelector('#upload-file');
 const uploadOverlayElement = document.querySelector('.img-upload__overlay');
-const uploadedImageElement = document.querySelector('.img-upload__preview img'); // *
+const uploadedImageElement = document.querySelector('.img-upload__preview img');
 const imageScaleValueElement = document.querySelector('.scale__control--value');
 const imageEffectLevelElement = document.querySelector('.effect-level');
 const imageEffectLevelValueElement = document.querySelector('.effect-level__value');
@@ -48,7 +50,7 @@ function closeUploadWindow() {
   bodyElement.removeEventListener('keydown', onCloseFromKeyboard);
 }
 
-function previewImage() {
+function uploadChosenImage() {
   const image = uploadControlElement.files[0];
   const imageName = image.name.toLowerCase();
 
@@ -56,12 +58,15 @@ function previewImage() {
 
   if(match) {
     uploadedImageElement.src = URL.createObjectURL(image);
+  } else {
+    showAlert(INCORRECT_IMAGE_FORMAT);
+    closeUploadWindow();
   }
 }
 
 uploadControlElement.addEventListener('change', () => {
   openUploadWindow();
-  previewImage();
+  uploadChosenImage();
 });
 
 export {closeUploadWindow};
