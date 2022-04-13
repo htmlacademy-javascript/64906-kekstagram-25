ffc177afff7a1c2e21c674def1b874829d0eeb67
Вячеслав Николаев <slavkapiyavka@yandex.ru>
2022-04-14 02:49:06 +0300

    Реализует загрузку комментариев по пять штук

--- js/fullsize-image.js
+++ js/fullsize-image.js
@@ -1,15 +1,19 @@
 import {isEscapeKey} from './utils.js';
 
+const COMMENTS_CHUNK_COUNT = 5;
+
 const bodyElement = document.body;
 const overlayElement = document.querySelector('.big-picture');
 const largeImageElement = overlayElement.querySelector('.big-picture__img img');
 const largeImageLikesElement = overlayElement.querySelector('.likes-count');
 const largeImageCommentsListElement = overlayElement.querySelector('.social__comments');
+const renderedCommentsCountElement = document.querySelector('.rendered-comments');
 const commentsCounterElement = overlayElement.querySelector('.comments-count');
-const commentsCounterBlockElement = overlayElement.querySelector('.social__comment-count');
 const largeImageDescriptionElement = overlayElement.querySelector('.social__caption');
 const newCommentsLoaderElement = overlayElement.querySelector('.comments-loader');
 const closeBtnElement = overlayElement.querySelector('.big-picture__cancel');
+
+let postComments = [];
 
 closeBtnElement.addEventListener('click', closeModal);
 
@@ -22,7 +26,6 @@ function onCloseFromKeyboard(evt) {
 
 function setThumbnailsHandlers(posts) {
   const picturesThumbnails = document.querySelectorAll('.picture');
-
   picturesThumbnails.forEach((thumbnail, i) => {
     thumbnail.addEventListener('click', (evt) => {
       evt.preventDefault();
@@ -31,7 +34,19 @@ function setThumbnailsHandlers(posts) {
   });
 }
 
+newCommentsLoaderElement.addEventListener(
+  'click',
+  () => {
+    postComments.splice(0, COMMENTS_CHUNK_COUNT).forEach((comment) => renderComment(comment));
+    renderedCommentsCountElement.textContent = largeImageCommentsListElement.querySelectorAll('.social__comment').length;
+  }
+);
+
 function renderComment({avatar, name, message}) {
+  newCommentsLoaderElement.classList.add('hidden');
+  if(postComments.length) {
+    newCommentsLoaderElement.classList.remove('hidden');
+  }
   const commentItem = document.createElement('li');
   commentItem.classList.add('social__comment');
   commentItem.innerHTML = `
@@ -42,7 +57,6 @@ function renderComment({avatar, name, message}) {
             width="35" height="35">
         <p class="social__text">${message}</p>
 `;
-
   largeImageCommentsListElement.appendChild(commentItem);
 }
 
@@ -52,14 +66,14 @@ function openModal({url, likes, comments, description}) {
   largeImageElement.src = url;
   largeImageLikesElement.textContent = likes;
   commentsCounterElement.textContent = comments.length;
-  largeImageCommentsListElement.innerHTML = '';
+  largeImageCommentsListElement.querySelectorAll('.social__comment')
+    .forEach((commentElement) => commentElement.remove());
   largeImageDescriptionElement.textContent = description;
-  commentsCounterBlockElement.classList.add('hidden');
-  newCommentsLoaderElement.classList.add('hidden');
-
-  comments.forEach((comment) => {
+  postComments = [...comments];
+  postComments.splice(0, COMMENTS_CHUNK_COUNT).forEach((comment) => {
     renderComment(comment);
   });
+  renderedCommentsCountElement.textContent = largeImageCommentsListElement.querySelectorAll('.social__comment').length;
   bodyElement.addEventListener('keydown', onCloseFromKeyboard);
 }
 
